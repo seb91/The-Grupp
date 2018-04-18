b3b8c453d8b0aa00b5dce7b3ea3b4402d558a2cc
AllexNordgren <allexnordgren@outlook.com>
2018-04-18 11:21:39 +0200

    Creating Weapon class

---
+++ Game/src/main/java/Terrain.java
@@ -0,0 +1,45 @@
+public class Terrain {
+    private int posX;
+    private int posY;
+    private int width;
+    private int height;
+
+    public Terrain(int posX, int posY, int width, int height) {
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
