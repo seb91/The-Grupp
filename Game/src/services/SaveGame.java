e025b3f134804bfb7e3199637aaa3ab4beda6ee4
AllexNordgren <allexnordgren@outlook.com>
2018-04-25 15:03:41 +0200

    adding functionality to read and write txt files

--- Game/src/services/SaveGame.java
+++ Game/src/services/SaveGame.java
@@ -1,12 +1,10 @@
 package services;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 
-public class SaveGame {
+public final class SaveGame {
 
-    private void saveGame(Character character, int lvl, int saveSlot){
+    public static void saveGame(Character character, int lvl, int saveSlot){
         PrintWriter writer = null;
         try {
             writer = new PrintWriter("save" + saveSlot + ".txt", "UTF-8");
@@ -18,5 +16,56 @@ public class SaveGame {
         writer.println(character.toString());
         writer.println(lvl);
         writer.close();
+    }
+
+    public static void saveGame(String test){
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("save" + ".txt", "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        writer.println(test);
+        writer.println(123);
+        writer.close();
+    }
+
+    public static void createGame(int saveSlot){
+        // The name of the file to open.
+        String fileName = "save" + saveSlot + ".txt";
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
     }
 }
