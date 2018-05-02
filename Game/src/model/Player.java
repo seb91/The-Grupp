96d5673ceb35d2655fe1979a2d3b0c8fc402dfec
Emberg <Emberg94@gmail.com>
2018-05-02 09:15:10 +0200

    Basic model structure
    Basic model structure, needs further implementation

---
+++ Game/src/model/Player.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Player extends Entity{
+    public Player(int posX, int posY, int width, int height) {
+        super(posX, posY, width, height);
+    }
+}
---
+++ Game/src/model/Projectile.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Projectile extends Entity{
+    public Projectile(int posX, int posY, int width, int height) {
+        super(posX, posY, width, height);
+    }
+}
