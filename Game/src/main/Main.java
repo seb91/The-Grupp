2da34c9393050ec01b38366b0d15c1298ea35a6d
Alexander Nordgren <allexnordgren@outlook.com>
2018-05-11 11:43:47 +0200

    fixing the save function to return the level

--- Game/src/main/Main.java
+++ Game/src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
 import controller.*;
+import services.SaveGame;
+
+import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main (String[] args) {
         new Game().run();
     }
 }
--- Game/src/services/SaveGame.java
+++ Game/src/services/SaveGame.java
@@ -1,27 +1,32 @@
 package services;
 
 import java.io.*;
+import java.util.Scanner;
 
 public final class SaveGame {
 
-    public static void saveGame(Character character, int lvl, int saveSlot){
+    public static void saveGame(int lvl, int saveSlot) {
         PrintWriter writer = null;
         try {
-            writer = new PrintWriter("save" + saveSlot + ".txt", "UTF-8");
+            writer = new PrintWriter("./saves/save" + saveSlot + ".txt", "UTF-8");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        writer.println(character.toString());
+        //writer.println(character.toString());
+        if (lvl < createGame(saveSlot)){
+            lvl = createGame(saveSlot);
+        }
+        writer.println("level");
         writer.println(lvl);
         writer.close();
     }
 
-    public static void saveGame(String test){
+    public static void saveGame(String test) {
         PrintWriter writer = null;
         try {
-            writer = new PrintWriter("save" + ".txt", "UTF-8");
+            writer = new PrintWriter("./saves/save" + "1" +".txt", "UTF-8");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
@@ -32,40 +37,17 @@ public final class SaveGame {
         writer.close();
     }
 
-    public static void createGame(int saveSlot){
-        // The name of the file to open.
-        String fileName = "save" + saveSlot + ".txt";
-
-        // This will reference one line at a time
-        String line = null;
-
-        try {
-            // FileReader reads text files in the default encoding.
-            FileReader fileReader =
-                    new FileReader(fileName);
-
-            // Always wrap FileReader in BufferedReader.
-            BufferedReader bufferedReader =
-                    new BufferedReader(fileReader);
-
-            while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+    public static int createGame(int saveSlot){
+        int lvl = 1;
+        try (Scanner sc = new Scanner(new File("./saves/save" + saveSlot + ".txt"))) {
+            while (sc.hasNext()) {
+                if (sc.next().equals("level")) {
+                    lvl = Integer.parseInt(sc.next());
+                }
             }
-
-            // Always close files.
-            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        catch(FileNotFoundException ex) {
-            System.out.println(
-                    "Unable to open file '" +
-                            fileName + "'");
-        }
-        catch(IOException ex) {
-            System.out.println(
-                    "Error reading file '"
-                            + fileName + "'");
-            // Or we could just do this:
-            // ex.printStackTrace();
-        }
+        return lvl;
     }
 }
